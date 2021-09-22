589be3111c8eb6ba0f3c9a486190eac8987c50ae
Andrea Freeman <afreeman5566@gmail.com>
2021-09-22 11:57:57 -0500

    commit tree 48beffd0ca5ee4e68ac601805d9791d8953c046a

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+ 
+// distance in blocks
+function distanceFromHqInBlocks(pickupLocationinft){
+    return Math.abs(pickupLocationinft - 42)
+}
+
+
+//distance from the HQ in feet
+ function distanceFromHqInFeet(block) {
+  let distance = distanceFromHqInBlocks(block)
+    return distance * 264
+ }
+
+function distanceTravelledInFeet(start, finish){
+  
+return Math.abs(start-finish)*264
+}
+
+function calculatesFarePrice(start, destination){
+    let fareDistance = distanceTravelledInFeet( start, destination)
+
+    if(fareDistance<=400){
+         return 0
+        }
+    else if(fareDistance>400, fareDistance<=2000){
+        return .02*(fareDistance-400)
+        } 
+    else if (fareDistance>2000, fareDistance<=2500){
+        return 25
+    }
+    else if (fareDistance>2500){
+        return "cannot travel that far"
+    }
+
+
+    }
+
+
